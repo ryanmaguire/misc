81a77a54ae782f69a98b5d06f9901c13e4bf202b
ryanmaguire <rmaguire@protonmail.com>
2023-10-09 13:18:11 -0400

    More old scripts and bash functions.

---
+++ bash/rsync_examples.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+################################################################################
+#                                   LICENSE                                    #
+################################################################################
+#   This file is free software: you can redistribute it and/or modify          #
+#   it under the terms of the GNU General Public License as published by       #
+#   the Free Software Foundation, either version 3 of the License, or          #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   This file is distributed in the hope that it will be useful,               #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU General Public License for more details.                               #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License          #
+#   along with this file.  If not, see <https://www.gnu.org/licenses/>.        #
+################################################################################
+checkserver() {
+    rsync -avhxn --progress --delete /path/to/site_data/ user@server:public_html/
+}
+
+syncserver() {
+    rsync -avhx --progress --delete /path/to/site_data/ user@server:public_html/
+}
+
---
+++ bash/tar_with_xz.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+################################################################################
+#                                   LICENSE                                    #
+################################################################################
+#   This file is free software: you can redistribute it and/or modify          #
+#   it under the terms of the GNU General Public License as published by       #
+#   the Free Software Foundation, either version 3 of the License, or          #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   This file is distributed in the hope that it will be useful,               #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU General Public License for more details.                               #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License          #
+#   along with this file.  If not, see <https://www.gnu.org/licenses/>.        #
+################################################################################
+#   Purpose:                                                                   #
+#      	Creates a compressed tape archive using tar and xz.                    #
+################################################################################
+tarmystuff() {
+    TARFIL="${1}.tar.xz"
+    DIR="${1}"
+    tar -c -I 'xz -9 -T0' -f $TARFIL $DIR
+}
