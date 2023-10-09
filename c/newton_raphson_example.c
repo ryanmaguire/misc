16ff9dd46b332715931b96ec902d2c98c4893398
ryanmaguire <rmaguire@protonmail.com>
2023-10-09 10:40:44 -0400

    Replacing tabs with spaces.

--- c/newton_raphson_example.c
+++ c/newton_raphson_example.c
@@ -50,16 +50,16 @@ static double newton(double x, function f, function f_prime)
         x -= dx;
 
         /*  Avoid an infinite loop, update the number of iterations performed.*/
-		++iters;
+        ++iters;
 
         /*  Break out of the loop if this takes too long.                     */
         if (iters == MAX_ITERS)
             break;
 
     /*  Abort the computation once the Newton-Raphson factor is small.        */
-	} while(fabs(dx) > EPS);
+    } while(fabs(dx) > EPS);
 
-	return x;
+    return x;
 }
 /*  End of newton.                                                            */
 
@@ -83,36 +83,36 @@ static double d_x_12_minus_2(double x){return 12.0*pow(x, 11.0);}
 int main(void)
 {
     /*  Variable for the guess point, and the computed root.                  */
-	double x, root;
+    double x, root;
 
     /*  Use Newton-Raphson to compute sqrt(2).                                */
     x = 2.0;
-	root = newton(x, x_square, d_x_square);
-	printf("The Square Root of Two: %0.16F\n", root);
-	printf("Exact:                  1.414213562373095048801688724\n\n");
+    root = newton(x, x_square, d_x_square);
+    printf("The Square Root of Two: %0.16F\n", root);
+    printf("Exact:                  1.414213562373095048801688724\n\n");
 
     /*  Use Newton-Raphson to compute pi.                                     */
-	x = 3.0;
-	root = newton(x, sin, cos);
-	printf("The Value of Pi: %0.16F\n", root);
-	printf("Exact:           3.14159265358979323846\n\n");
+    x = 3.0;
+    root = newton(x, sin, cos);
+    printf("The Value of Pi: %0.16F\n", root);
+    printf("Exact:           3.14159265358979323846\n\n");
 
     /*  Use Newton-Raphson to compute exp(1).                                 */
-	x = 3.0;
-	root = newton(x, log_minus_1, d_log_minus_1);
-	printf("Euler's Number (e): %0.16F\n", root);
-	printf("Exact:              2.71828182845904523536028\n\n");
+    x = 3.0;
+    root = newton(x, log_minus_1, d_log_minus_1);
+    printf("Euler's Number (e): %0.16F\n", root);
+    printf("Exact:              2.71828182845904523536028\n\n");
 
     /*  Use Newton-Raphson to compute the golden ratio.                       */
-	x = 2.0;
-	root = newton(x, golden_ratio, d_golden_ratio);
-	printf("The Golden Ratio: %0.16F\n", root);
-	printf("Exact:            1.618033988749894848204\n\n");
+    x = 2.0;
+    root = newton(x, golden_ratio, d_golden_ratio);
+    printf("The Golden Ratio: %0.16F\n", root);
+    printf("Exact:            1.618033988749894848204\n\n");
 
     /*  Use Newton-Raphson to compute the music number, 2^{1/12}.             */
-	x = 1.0;
-	root = newton(x, x_12_minus_2, d_x_12_minus_2);
-	printf("The Music Number is: %0.16F\n", root);
-	printf("Exact:               1.059463094359295264561825\n");
+    x = 1.0;
+    root = newton(x, x_12_minus_2, d_x_12_minus_2);
+    printf("The Music Number is: %0.16F\n", root);
+    printf("Exact:               1.059463094359295264561825\n");
 }
 /*  End of main.                                                              */
