4f5cd4046460fe6e02fa4aabc03ed3f13d592e75
ryanmaguire <rmaguire@protonmail.com>
2023-10-10 08:28:53 -0400

    Adding shell script for downloading git repos from github.

---
+++ bash/git_clone_all_of_my_github_repos.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+################################################################################
+#                                   LICENSE                                    #
+################################################################################
+#   This file is free software: you can redistribute it and/or modify          #
+#   it under the terms of the GNU General Public License as published by       #
+#   the Free Software Foundation, either version 3 of the License, or          #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   This file is distributed in the hope that it will be useful,               #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU General Public License for more details.                               #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License          #
+#   along with this file.  If not, see <https://www.gnu.org/licenses/>.        #
+################################################################################
+#   Purpose:                                                                   #
+#      	Clones all of my github repos into the current directory.              #
+################################################################################
+GITHUB_USERNAME=ryanmaguire
+curl "https://api.github.com/users/$GITHUB_USERNAME/repos?per_page=1000" | \
+    grep -w clone_url | \
+    grep -o '[^"]\+://.\+.git' | \
+    xargs -L1 git clone
