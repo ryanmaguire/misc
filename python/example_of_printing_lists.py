ab7de1e1886571e520d65d01e8d3937086835045
ryanmaguire <rmaguire@protonmail.com>
2024-05-15 08:32:32 -0400

    More sample code for learning Python.

---
+++ python/example_of_printing_lists.py
@@ -0,0 +1,34 @@
+"""
+################################################################################
+#                                   LICENSE                                    #
+################################################################################
+#   This file is free software: you can redistribute it and/or modify          #
+#   it under the terms of the GNU General Public License as published by       #
+#   the Free Software Foundation, either version 3 of the License, or          #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   This file is distributed in the hope that it will be useful,               #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU General Public License for more details.                               #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License          #
+#   along with this file.  If not, see <https://www.gnu.org/licenses/>.        #
+################################################################################
+#   Purpose:                                                                   #
+#       Provides more examples of using lists.                                 #
+################################################################################
+"""
+
+NUMBERS = []
+STRINGS = []
+NAMES = ["John", "Eric", "Jessica"]
+NUMBERS.append(1)
+NUMBERS.append(2)
+NUMBERS.append(3)
+STRINGS.append("hello")
+STRINGS.append("world")
+SECOND_NAME = NAMES[1]
+print(NUMBERS)
+print(STRINGS)
+print("Second name in name list is " + SECOND_NAME)
