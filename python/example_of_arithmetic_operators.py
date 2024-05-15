7e95eef65882e327ce7ec74387d403f2e139f32f
ryanmaguire <rmaguire@protonmail.com>
2024-05-15 14:39:28 -0400

    Adding author and date stamp.

--- python/example_of_arithmetic_operators.py
+++ python/example_of_arithmetic_operators.py
@@ -18,6 +18,9 @@
 #   Purpose:                                                                   #
 #       Provides an example of using arithmetic operators in Python.           #
 ################################################################################
+#   Author:     Ryan Maguire                                                   #
+#   Date:       2024/05/15                                                     #
+################################################################################
 """
 
 # We can use basic addition, multiplication, and division.
